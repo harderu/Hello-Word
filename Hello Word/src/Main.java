dfd305c31a8f01a55860ba8218aaf322348771c9
harderu <live.imho@gmail.com>
2025-01-31 23:27:36 +0300

    изменения в ветке

--- Hello Word/src/Main.java
+++ Hello Word/src/Main.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello People");
-        Scanner sr = new Scanner(System.in);
+
 
     }
 }
