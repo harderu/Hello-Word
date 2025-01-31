fd4ea005f264984f49ea193ee0937cc6bf154535
harderu <live.imho@gmail.com>
2025-01-31 23:09:10 +0300

    Создан класс Main

---
+++ Hello Word/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+        Scanner sr = new Scanner(System.in);
+
+    }
+}
+
+
